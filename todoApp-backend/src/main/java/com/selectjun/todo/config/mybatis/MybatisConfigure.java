91fbbb5dcfefc9b13ede2e721dc469efdbddba1e
selectjun <selectjun99@inpix.com>
2020-11-26 14:37:06 +0900

    MyBatis 설정.

---
+++ todoApp-backend/src/main/java/com/selectjun/todo/config/mybatis/MybatisConfigure.java
@@ -0,0 +1,33 @@
+package com.selectjun.todo.config.mybatis;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan({"com.selectjun.todo.config.security.mapper", "com.selectjun.todo.mapper"})
+public class MybatisConfigure {
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+
+        sqlSessionFactory.setDataSource(dataSource);
+        sqlSessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:/mapper/**/*.xml"));
+
+        return sqlSessionFactory.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSession(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+}
+
