82118d47136b1e7435cebccd05fe99d4232ed22c
selectjun <selectjun99@inpix.com>
2020-11-30 15:15:53 +0900

    Mybatis 설정 변경.

--- todoApp-backend/src/main/java/com/selectjun/todo/TodoApplication.java
+++ todoApp-backend/src/main/java/com/selectjun/todo/TodoApplication.java
@@ -1,9 +1,12 @@
 package com.selectjun.todo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
+@MapperScan({"com.selectjun.todo.config.security.mapper", "com.selectjun.todo.mapper"})
 public class TodoApplication {
 
     public static void main(String[] args) {
