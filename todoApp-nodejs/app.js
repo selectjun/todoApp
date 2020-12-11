cb7f7bf17150eb3e091d7a0dd87efd2f3e92435f
selectjun <selectjun@gmail.com>
2020-12-11 16:25:58 +0900

    CORS 조치

--- todoApp-nodejs/app.js
+++ todoApp-nodejs/app.js
@@ -11,6 +11,14 @@ const tokenRotuer = require("./routes/token");
 
 // CORS 처리
 app.use(cors());
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Headers", "X-AUTH-TOKEN");
+  res.header("Access-Control-Expose-Headers", "X-AUTH-TOKEN");
+  res.header("Access-Control-Allow-Methods", "*");
+  next();
+});
 
 app.use("/", indexRouter);
 app.use("/api/user", userRouter);
