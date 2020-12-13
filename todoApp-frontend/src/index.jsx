3e4d74c1bd5ee5bc924a6ea5a055b8745bed49f3
selectjun <selectjun@gmail.com>
2020-12-13 21:58:17 +0900

    Redux 추가. #2

--- todoApp-frontend/src/index.jsx
+++ todoApp-frontend/src/index.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
+import rootReducer from "./store/modules"
+
+import "./reset.scss";
+import "./common.scss";
+
+import App from "./App";
 import Login from "./pages/Login";
 
+const store = createStore(rootReducer);
+console.log(store.getState());
+
 if (sessionStorage.getItem("xAuthToken")) {
-  ReactDOM.render(<App />, document.getElementById("root"));
+  ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
 } else {
-  ReactDOM.render(<Login />, document.getElementById("root"));
+  ReactDOM.render(<Provider store={store}><Login /></Provider>, document.getElementById("root"));
 }
