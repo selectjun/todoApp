ddb1a76b2d4cf41bd898e4f1cf3db6529e18de1d
selectjun <selectjun@gmail.com>
2020-12-22 18:08:53 +0900

    Google material icons 추가.

--- todoApp-frontend/src/components/Aside/Aside.jsx
+++ todoApp-frontend/src/components/Aside/Aside.jsx
@@ -4,29 +4,31 @@ import { Link } from "react-router-dom";
 import "./aside.scss"
 
 const Aside = () => {
+  const onToggleMenu = (e) => {
+    e.preventDefault();
+    document.querySelector(".container").classList.toggle("on");
+  }
+
+  const logout = (e) => {
+    e.preventDefault();
+    if (confirm("정말로 로그아웃을 하시겠습니까?")) {
+      sessionStorage.setItem("xAuthToken", null);
+      location.href = "/login";
+    }
+  }
+  
   return (
     <aside className="aside">
+      <button type="button" id="menuBtn" onClick={onToggleMenu}>
+        <span className="material-icons">menu</span>
+      </button>
       <ul>
-        <li><a
-          href="#"
-          onClick={(e) => {
-            e.preventDefault();
-            document.querySelector(".container").classList.toggle("on");
-          }}>메뉴</a></li>
         <li><Link to="/todo">TODOS</Link></li>
         <li><Link to="/user">회원정보</Link></li>
       </ul>
       <ul>
         <li>
-          <a
-            href="#"
-            onClick={(e) => {
-              e.preventDefault();
-              if (confirm("정말로 로그아웃을 하시겠습니까?")) {
-                sessionStorage.setItem("xAuthToken", null);
-                location.href = "/login";
-              }
-            }}>logout</a>
+          <a href="#" onClick={logout}>logout</a>
         </li>
       </ul>
     </aside>
