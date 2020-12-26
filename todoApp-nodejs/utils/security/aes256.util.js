d4f09ce0b979d7f63ac384bd1405ae8d2feaf2df
selectjun <selectjun@gmail.com>
2020-12-26 21:32:56 +0900

    aes256 모듈에 Null이나 빈값이 들어온 경우, 예외처리

--- todoApp-nodejs/utils/security/aes256.util.js
+++ todoApp-nodejs/utils/security/aes256.util.js
@@ -6,9 +6,17 @@ const cipher = aes256.createCipher(config.aes256.key);
 
 module.exports = {
   encrypt: (plaintext) => {
-    return cipher.encrypt(plaintext);
+    try {
+      return cipher.encrypt(plaintext);
+    } catch(err) {
+      new Error(`AES-256 암화화 과정 중, 에러가 발생하였습니다\n${err}`);
+    }
   },
   decrypt: (plaintext) => {
-    return cipher.decrypt(plaintext);
+    try {
+      return cipher.decrypt(plaintext);
+    } catch(err) {
+      new Error(`AES-256 복화화 과정 중, 에러가 발생하였습니다\n${err}`);
+    }
   }
 }
