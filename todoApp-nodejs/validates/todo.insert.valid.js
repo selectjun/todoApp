9d84fdcce28e2180295000c8c01e7a3ee62d4ebf
selectjun <selectjun@gmail.com>
2021-01-01 14:43:35 +0900

    To Do 유효성 체크 수정

--- todoApp-nodejs/validates/todo.insert.valid.js
+++ todoApp-nodejs/validates/todo.insert.valid.js
@@ -1,8 +1,19 @@
 const { check } = require('express-validator');
 
-const todo = [
+const todoInsertValid = [
   check("title").trim()
-    .not().isEmpty().withMessage("제목을 입력해주세요")
+    .not().isEmpty().withMessage("제목을 입력해주세요"),
+  check("startAt")
+    .custom((value, { req }) => /(\d{4})\-(\d{2})\-(\d{2})T(\d{2}):(\d{2}):(\d{2})\.(\d{3})Z/.test(value) || value === "null" || value === "undefined" || value === "")
+    .withMessage("[startAt] 타입이 맞지 않습니다"),
+  check("endAt")
+    .custom((value, { req }) => /(\d{4})\-(\d{2})\-(\d{2})T(\d{2}):(\d{2}):(\d{2})\.(\d{3})Z/.test(value) || value === "null" || value === "undefined" || value === "")
+    .withMessage("[endAt] 타입이 맞지 않습니다"),
+  check("fileId")
+    .custom((value, { req }) => Number.isInteger(parseInt(value)) || value === "null" || value === "undefined" || value === "")
+    .withMessage("[fileId] 타입이 맞지 않습니다"),
+  check("isComplete")
+    .isBoolean().withMessage("[isComplete] 타입이 맞지 않습니다")
 ];
 
-module.exports = todo;
+module.exports = todoInsertValid;
--- todoApp-nodejs/validates/todo.update.valid.js
+++ todoApp-nodejs/validates/todo.update.valid.js
@@ -1,10 +1,21 @@
 const { check } = require('express-validator');
 
-const todo = [
+const todoUpdateValid = [
   check("todoId")
     .not().isEmpty().withMessage("[todoId] 값이 존재하지 않습니다"),
   check("title").trim()
-    .not().isEmpty().withMessage("제목을 입력해주세요")
+    .not().isEmpty().withMessage("제목을 입력해주세요"),
+  check("startAt")
+    .custom((value, { req }) => /(\d{4})\-(\d{2})\-(\d{2})T(\d{2}):(\d{2}):(\d{2})\.(\d{3})Z/.test(value) || value === "null" || value === "undefined" || value === "")
+    .withMessage("[startAt] 타입이 맞지 않습니다"),
+  check("endAt")
+    .custom((value, { req }) => /(\d{4})\-(\d{2})\-(\d{2})T(\d{2}):(\d{2}):(\d{2})\.(\d{3})Z/.test(value) || value === "null" || value === "undefined" || value === "")
+    .withMessage("[endAt] 타입이 맞지 않습니다"),
+  check("fileId")
+    .custom((value, { req }) => Number.isInteger(parseInt(value)) || value === "null" || value === "undefined" || value === "")
+    .withMessage("[fileId] 타입이 맞지 않습니다"),
+  check("isComplete")
+    .isBoolean().withMessage("[isComplete] 타입이 맞지 않습니다")
 ];
 
-module.exports = todo;
+module.exports = todoUpdateValid;
