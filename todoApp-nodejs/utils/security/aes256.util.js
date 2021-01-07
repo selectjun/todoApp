48570f1aad2d9df54b4bb7598e30196cfd313dbb
selectjun <selectjun@gmail.com>
2021-01-07 21:05:00 +0900

    기존 aes256 모듈이 원하는 구현 방식과 달라 교체.

--- todoApp-nodejs/utils/security/aes256.util.js
+++ todoApp-nodejs/utils/security/aes256.util.js
@@ -1,20 +1,36 @@
-const aes256 = require('aes256');
+const Crypto = require("crypto");
 
 const config = require("../../config/config.json").dev;
 
-const cipher = aes256.createCipher(config.aes256.key);
+const encryptWithAES256 = (secretKey, plainText) => {
+  const secretKeyToByteArray = Buffer.from(secretKey, "utf8");
+  const ivParameter = Buffer.from(secretKey.slice(0, 16));
+  const cipher = Crypto.createCipheriv("aes-256-cbc", secretKeyToByteArray, ivParameter);
+  let encryptedValue = cipher.update(plainText, "utf8", "base64");
+  encryptedValue += cipher.final("base64");
+  return encryptedValue;
+};
+
+const decryptWithAES256 = (secretKey, encryptedText) => {
+  const secretKeyToBufferArray = Buffer.from(secretKey, "utf8");
+  const ivParameter = Buffer.from(secretKey.slice(0, 16));
+  const cipher = Crypto.createDecipheriv("aes-256-cbc", secretKeyToBufferArray, ivParameter);
+  let decryptedValue = cipher.update(encryptedText, "base64", "utf8");
+  decryptedValue += cipher.final("utf8");
+  return decryptedValue;
+};
 
 module.exports = {
   encrypt: (plaintext) => {
     try {
-      return cipher.encrypt(plaintext);
+      return encryptWithAES256(config.aes256.key, plaintext);
     } catch(err) {
-      new Error(`AES-256 암화화 과정 중, 에러가 발생하였습니다\n${err}`);
+      new Error(`AES-256 복화화 과정 중, 에러가 발생하였습니다\n${err}`);
     }
   },
   decrypt: (plaintext) => {
     try {
-      return cipher.decrypt(plaintext);
+      return decryptWithAES256(config.aes256.key, plaintext);
     } catch(err) {
       new Error(`AES-256 복화화 과정 중, 에러가 발생하였습니다\n${err}`);
     }
